cba1141abe57c832dc3c00047c6932acc951fe6e
Matt Rasband <matt.rasband@gmail.com>
2018-03-04 07:35:23 -0700

    Remove duplicated client usage in http clients

--- littlebird/web.py
+++ littlebird/web.py
@@ -12,9 +12,14 @@ from .urls import UrlEnum
 
 
 class AbstractAsyncHttpClient(ABC):
+    __slots__ = ('_loop', '_client')
+
     def __init__(self, *, loop: Optional[BaseEventLoop] = None):
         self._loop = loop or asyncio.get_event_loop()
         self._client = ClientSession(loop=self._loop)
+
+    def __del__(self):
+        self._loop.run_until_complete(self._client.close())
 
     @abstractmethod
     async def stream(self, url: UrlEnum, body=None,
@@ -31,22 +36,18 @@ class OAuth1HttpClient(AbstractAsyncHttpClient):
     """Client for handling oauth1 related actions. This is
     required for any endpoints that require the user-context
     that is unattended or application-only endpoints."""
-    __slots__ = ('_client', '_oauth_client', '_loop')
+    __slots__ = ('_oauth_client',)
 
     def __init__(self, consumer_key: str, consumer_secret: str, *,
                  access_token: Optional[str] = None,
                  access_token_secret: Optional[str] = None):
-        self._loop = asyncio.get_event_loop()
-        self._client = ClientSession(loop=self._loop)
+        super().__init__()
         self._oauth_client = OAuth1Client(
             consumer_key,
             client_secret=consumer_secret,
             resource_owner_key=access_token,
             resource_owner_secret=access_token_secret
         )
-
-    def __del__(self):
-        self._loop.run_until_complete(self._client.close())
 
     async def stream(self, url, body=None, method='post'):
         url, headers, body = self._oauth_client.sign(
